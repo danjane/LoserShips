fb086c7eb697469a2272b0e3cbe4db5835a23e3e
jdtjane <jdtjane@gmail.com>
2018-03-19 23:52:01 +0100

    Initial work into functions, a loader for the MarineTraffic csvs

--- boat.py
+++ boat.py
@@ -14,57 +14,88 @@
 import pyproj
 import datetime
 import numpy as np
+import pandas as pd
 
-# https://stackoverflow.com/questions/8776414/python-datetime-to-matlab-datenum
+
 def datetime2matlabdn(dt):
+    # https://stackoverflow.com/questions/8776414/python-datetime-to-matlab-datenum
     mdn = dt + datetime.timedelta(days=366)
     frac_seconds = (dt-datetime.datetime(dt.year, dt.month, dt.day, 0, 0, 0)).seconds / (24.0 * 60.0 * 60.0)
     frac_microseconds = dt.microsecond / (24.0 * 60.0 * 60.0 * 1000000.0)
     return mdn.toordinal() + frac_seconds + frac_microseconds
 
 
-# point A: London
-startlat = 51.5074
-startlong = 0.1278
-starttime = datetime.datetime.now()
+def interpolate_positions_times_velocities(ptv_matrix):
+    interpolated_ptvs = []
+    for idx in range(ptv_matrix.shape[0]-1):
+        # Loop down pairs doing a linear interpolation
+        startlat, startlong, starttime = ptv_matrix[idx, :3]
+        endlat, endlong, endtime = ptv_matrix[idx+1, :3]
 
-# point B: NewYork
-endlat = 40.730610
-endlong = -73.935242
-endtime = starttime + datetime.timedelta(days=9)
+        interpolated_ptvs.append(
+            interpolate_linear_position(startlat, startlong, starttime, endlat, endlong, endtime))
 
-
-########### Intermediate locations
-g = pyproj.Geod(ellps='WGS84')
-(az12, az21, dist) = g.inv(startlong, startlat, endlong, endlat)
-
-dist /= 1000. #switch to km
-# calculate line string along path with segments <= 1 km
-lonlats = g.npts(startlong, startlat, endlong, endlat,
-                 1 + int(dist))
-
-# npts doesn't include start/end points, so prepend/append them
-lonlats.insert(0, (startlong, startlat))
-lonlats.append((endlong, endlat))
-lonlats = np.asarray(lonlats) # warning! still y-x (=lon,lat)
+    return np.concatenate(interpolated_ptvs, axis=1)
 
 
-########### Intermediate times
-d_time = endtime - starttime
-n_points = lonlats.shape[0]
-i_time = d_time / (n_points - 1)
-times = np.zeros((n_points,1))
-for i in range(n_points):
-    times[i] = datetime2matlabdn(starttime + i_time*i)
+def interpolate_linear_position(startlat, startlong, starttime, endlat, endlong, endtime):
+    # Intermediate locations
+    g = pyproj.Geod(ellps='WGS84')
+    (az12, az21, dist) = g.inv(startlong, startlat, endlong, endlat)
+
+    dist /= 1000.  # switch to km
+    # calculate line string along path with segments <= 1 km
+    lonlats = g.npts(startlong, startlat, endlong, endlat,
+                     1 + int(dist))
+
+    # npts doesn't include start/end points, so prepend/append them
+    lonlats.insert(0, (startlong, startlat))
+    lonlats.append((endlong, endlat))
+    lonlats = np.asarray(lonlats)  # warning! still y-x (=lon,lat)
+
+    # Intermediate times
+    n_points = lonlats.shape[0]
+    times = np.linspace(starttime, endtime, n_points)
+    # d_time = endtime - starttime
+    # i_time = d_time / (n_points - 1)
+    # times = np.zeros((n_points, 1))
+    # for i in range(n_points):
+    #     times[i] = datetime2matlabdn(starttime + i_time * i)
+
+    # Intermediate velocities
+    speed = dist / (times[-1] - times[0]) / 24.  # currently assuming constant speed
+    velocities = np.zeros((n_points, 2))
+    for i in range(n_points - 1):
+        (az12, az21, dist) = g.inv(lonlats[i, 0], lonlats[i, 1], lonlats[i + 1, 0], lonlats[i + 1, 1])
+        velocities[i, :] = [speed * np.sin(az12), speed * np.cos(az12)]
+    velocities[-1, :] = [speed * np.sin(az21), speed * np.cos(az21)]
+
+    # Output array
+    return np.concatenate((lonlats[:, [1, 0]], times[:, None], velocities), axis=1)
 
 
-########### Intermediate velocities
-speed = dist / (times[-1]-times[0]) / 24. #currently assuming constant speed
-velocities = np.zeros((n_points, 2))
-for i in range(n_points-1):
-    (az12, az21, dist) = g.inv(lonlats[i, 0], lonlats[i, 1], lonlats[i+1, 0], lonlats[i+1, 1])
-    velocities[i, :] = [speed * np.sin(az12), speed * np.cos(az12)]
-velocities[-1, :] = [speed * np.sin(az21), speed * np.cos(az21)]
+def load_MarineTraffic_csv(filepath):
+    full = pd.read_csv(filepath, skipinitialspace=True)
 
-########### Output array
-location_time_velocity = np.concatenate((lonlats[:, [1, 0]], times, velocities), axis=1)
+    # Convert timestamp from string to Matlab datenum
+    timestamp2matlabdn = lambda x: datetime2matlabdn(
+        datetime.datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'))
+    full['TIMESTAMP'] = full['TIMESTAMP'].apply(timestamp2matlabdn)
+
+    full = full.sort_values(by='TIMESTAMP', ascending=False)
+
+    # Now embarrassingly throw all the data away (for now)
+    return full[['LON', 'LAT', 'TIMESTAMP']].values
+
+
+# # point A: London
+# startlat = 51.5074
+# startlong = 0.1278
+# starttime = datetime.datetime.now()
+#
+# # point B: NewYork
+# endlat = 40.730610
+# endlong = -73.935242
+# endtime = starttime + datetime.timedelta(days=9)
+
+
